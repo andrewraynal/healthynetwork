cbdefa25920cd11d76565c422b0714f95148dea1
andrew <andrewraynal@me.com>
2014-06-18 14:37:54 -0400

    healthfinder looking/functioning better

--- app/scripts/services/healthfinderSvc.js
+++ app/scripts/services/healthfinderSvc.js
@@ -5,7 +5,7 @@ angular.module('ahealthynetworkApp')
 	var getResults;
 	 return {
 		async: function() {
-			getResults = $http.jsonp('http://www.healthfinder.gov/developers/Search.json?api_key=ldaefgwbdoehgifb&keyword=*&callback=JSON_CALLBACK').then(function(data){
+			getResults = $http.jsonp('http://www.healthfinder.gov/developers/Search.json?api_key=ldaefgwbdoehgifb&keyword=life&callback=JSON_CALLBACK').then(function(data){
 				return data;	
 			});
 
